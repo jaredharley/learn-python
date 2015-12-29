ca387bde4dc2b9337ce1bf3689f493634b7ab753
Jared Harley <jared@jaredharley.com>
2015-12-28 22:46:45 -0700

    Created basic templating
    Built basic templates from http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates

--- flask/microblog/app/views.py
+++ flask/microblog/app/views.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello there, world!"
+	user = {'nickname': 'Jared'}
+	# Array of example posts
+	posts = [
+		{
+			'author': {'nickname': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'nickname': 'Adam'},
+			'body': 'Star Wars was so cool.'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
