6501fff5d40f0aef9e0e683e6766b06b3c64fa07
Jared Harley <jared@jaredharley.com>
2015-12-29 22:05:38 -0700

    Added followers and friends
    Added followers and friends following http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-viii-followers-contacts-and-friends

--- flask/microblog/app/models.py
+++ flask/microblog/app/models.py
@@ -2,6 +2,11 @@ from app import db
 from flask.ext.login import UserMixin
 from hashlib import md5
 
+followers = db.Table('followers',
+	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	social_id = db.Column(db.String(64), unique=True)
@@ -10,18 +15,36 @@ class User(UserMixin, db.Model):
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime)
+	followed = db.relationship('User',
+							   secondary=followers,
+							   primaryjoin=(followers.c.follower_id == id),
+							   secondaryjoin=(followers.c.followed_id == id),
+							   backref=db.backref('followers', lazy='dynamic'),
+							   lazy='dynamic')
+
+	@staticmethod
+	def make_unique_nickname(nickname):
+		if User.query.filter_by(nickname=nickname).first() is None:
+			return nickname
+		version = 2
+		while True:
+			new_nickname = nickname + str(version)
+			if User.query.filter_by(nickname=new_nickname).first() is None:
+				break
+			version += 1
+		return new_nickname
 
 	@property
 	def is_authenticated(self):
-	    return True
+		return True
 
 	@property
 	def is_active(self):
-	    return True
+		return True
 	
 	@property
 	def is_anonymous(self):
-	    return False
+		return False
 	
 	def get_id(self):
 		try:
@@ -36,6 +59,25 @@ class User(UserMixin, db.Model):
 			avatarhash = md5(self.nickname.encode('utf-8')).hexdigest()
 		return 'http://www.gravatar.com/avatar/%s?d=identicon&s=%d' % (avatarhash, size)
 
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+			return self
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+			return self
+
+	def is_following(self, user):
+		return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+	def followed_posts(self):
+		return Post.query.join(
+			followers, (followers.c.followed_id == Post.user_id)).filter(
+				followers.c.follower_id == self.id).order_by(
+				Post.timestamp.desc())
+
 	def __repr__(self):
 		return '<User %r>' % (self.nickname)
 
@@ -47,3 +89,4 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return '<Post %r>' % (self.body)
+
