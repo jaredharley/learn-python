1b9be9fa9a08a3d5c0be5f3e2e07a910731d0b1e
Jared Harley <jared@jaredharley.com>
2016-01-01 13:24:51 -0700

    Added relative timestamps with moment.js
    http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xiii-dates-and-times

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -4,9 +4,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.mail import Mail
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.jinja_env.globals['momentjs'] = momentjs
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
